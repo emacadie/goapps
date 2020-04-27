7c39aa653d48d2e0a4e9cbe8295de57ef54277a1
Eric MacAdie <eric@MacAdie.net>
2020-04-26 21:27:20 -0500

    Added go routines

--- concurrency/code/main.go
+++ concurrency/code/main.go
@@ -13,21 +13,25 @@ func main() {
 	for i := 0; i < 10; i++ {
 		id := rnd.Intn(10) + 1
 		fmt.Println("Here is id: ", id)
-		
-		if b, ok := queryCache(id); ok {
-			fmt.Println("From cache: ")
-			fmt.Println(b)
-			continue
-		}
-		if b, ok := queryDatabase(id); ok {
-			fmt.Println("From database: ")
-			fmt.Println(b)
-			continue
-		}
-		fmt.Println("No Book found with ID: ", id)
+		go func (id int) {
+			if b, ok := queryCache(id); ok {
+				fmt.Println("From cache: ")
+				fmt.Println(b)
+				// continue
+			}
+		}(id)
+		go func (id int) {
+			if b, ok := queryDatabase(id); ok {
+				fmt.Println("From database: ")
+				fmt.Println(b)
+				// continue
+			}
+		}(id)
+		// fmt.Println("No Book found with ID: ", id)
 		time.Sleep(150 * time.Millisecond)
 		
 	}
+	time.Sleep(2 * time.Second)
 }
 
 func queryCache(id int) (Book, bool) {
