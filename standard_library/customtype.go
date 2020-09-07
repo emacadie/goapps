8ff856f3af16fb2e4e6d7f97d37b60f298a1f190
Eric MacAdie <eric@MacAdie.net>
2020-09-06 23:27:15 -0500

    Working with movies and media

---
+++ standard_library/customtype.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"media"
+)
+
+func main() {
+	fmt.Println( "My favorite movie" )
+	// old way, before we had the Catalogable interface
+	// myFave := media.NewMovie( "Farewell My Concubine", media.R, 43.2 )
+	var myFave media.Catalogable = &media.Movie{}
+	myFave.NewMovie( "Farewell My Concubine", media.R, 43.2 )
+	/* // the fields were mutable before
+	myFave.Title = "Farewell My Concubine"
+	myFave.Rating = media.R
+	myFave.BoxOffice = 43.2
+	fmt.Printf( "My favorite movie is: %s\n", myFave.Title )
+	fmt.Printf( "It was rated %v\n", myFave.Rating )
+	fmt.Printf( "It made %f at the box office\n", myFave.BoxOffice )
+    */
+	fmt.Printf( "My favorite movie is: %s\n", myFave.GetTitle() )
+	fmt.Printf( "It was rated %v\n", myFave.GetRating() )
+	fmt.Printf( "It made %f at the box office\n", myFave.GetBoxOffice() )
+
+
+	// try a different movie
+	myFave.SetTitle( "Austin Powers: The Spy Who Shagged Me" )
+	myFave.SetRating( media.PG13 )
+	myFave.SetBoxOffice( 21.3 )
+	fmt.Printf( "My favorite movie is: %s\n", myFave.GetTitle() )
+	fmt.Printf( "It was rated %v\n", myFave.GetRating() )
+	fmt.Printf( "It made %f at the box office\n", myFave.GetBoxOffice() )
+
+
+}
+
+
---
+++ standard_library/src/media/media.go
@@ -0,0 +1,74 @@
+package media
+
+import (
+	"strings"
+)
+
+
+type Catalogable interface {
+	NewMovie( title string, rating Rating, boxOffice float32 )
+	GetTitle() string
+	GetRating() string
+	GetBoxOffice() float32
+	SetTitle( newTitle string )
+	SetRating( newRating string )
+	SetBoxOffice( newBoxOffice float32 )
+}
+
+type Movie struct {
+	title string // mutable by other files if starting w/capital letter
+	rating Rating
+	boxOffice float32
+}
+type Rating string
+
+const (
+	R = "R (Restricted)"
+	G = "G (General audiences)"
+	PG = "PG (Parental Guidance)"
+	PG13 = "PG-13 (Parental Caution)"
+	NC17 = "NC-17 (No children under 17)"
+)
+
+func ( m *Movie ) NewMovie(argTitle string, rating Rating, boxOffice float32) {
+	m.title = argTitle
+	m.rating = rating
+	m.boxOffice = boxOffice
+}
+
+/*
+old NewMovie before Catalogable interface
+func NewMovie(argTitle string, rating Rating, boxOffice float32) Movie {
+	return Movie {
+		title: argTitle,
+		rating: rating,
+		boxOffice: boxOffice,
+	}
+}
+*/
+func ( m *Movie ) GetTitle() string {
+	return strings.Title( m.title )
+}
+func ( m *Movie ) GetRating() Rating {
+	return m.rating
+}
+func ( m *Movie ) GetBoxOffice() float32 {
+	return m.boxOffice
+}
+// to get the changes to stick, use a pointer-based receiver
+// we do need to change them for the setters to see changes,
+// but for consistency it is good to change the getters too
+// we also could have changed the methods to return a new immutable object
+func ( m *Movie ) SetTitle( newTitle string ) {
+	m.title = newTitle
+}
+
+func ( m *Movie ) SetRating( newRating Rating ) {
+	m.rating = newRating
+}
+
+func ( m *Movie ) SetBoxOffice( newBoxOffice float32 ) {
+	m.boxOffice = newBoxOffice
+}
+
+
