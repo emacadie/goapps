04586e7330763e3a0f4b3a360c0816fb868b13b2
Eric MacAdie <eric@MacAdie.net>
2020-11-05 23:21:26 -0600

    Adding logging statements, improving SQL

--- web_services/product/product.data.go
+++ web_services/product/product.data.go
@@ -7,10 +7,14 @@ import (
 	"log"
 	"os"
 	"sort"
+	"database/sql"
+	"strconv"
 	"sync"
 
 	"shelfunit.info/golang/inventoryservice/database"
 )
+
+const fileNameData = "In product.data."
 
 var productMap = struct {
 	sync.RWMutex
@@ -28,12 +32,12 @@ func init() {
 }
 
 func loadProductMap() (map[int]Product, error) {
-	fileName := "products.json"
-	_, err := os.Stat(fileName)
+	fileName2 := "products.json"
+	_, err := os.Stat(fileName2)
 	if os.IsNotExist(err) {
-		return nil, fmt.Errorf("file[%s] does not exist", fileName)
+		return nil, fmt.Errorf("file[%s] does not exist", fileName2)
 	}
-	file, _ := ioutil.ReadFile(fileName)
+	file, _ := ioutil.ReadFile(fileName2)
 	productList := make([]Product, 0)
 	err = json.Unmarshal([]byte(file), &productList)
 	if err != nil {
@@ -46,23 +50,52 @@ func loadProductMap() (map[int]Product, error) {
 	return prodMap, nil
 }
 
-func getProduct(productID int) *Product {
+func getProduct(productID int) (*Product, error) {
+	var funcName = fileNameData + "getProduct: "
+	log.Printf( funcName + " with product ID %d \n", productID )
+
+	row := database.DbConn.QueryRow( 
+		`select productId, manufacturer, sku, upc, pricePerUnit, quantityOnHand, productName 
+         from products where productId = $1`, productID )
+	product := &Product{}
+	err := row.Scan(&product.ProductID, 
+			&product.Manufacturer, 
+			&product.Sku, 
+			&product.Upc, 
+			&product.PricePerUnit, 
+			&product.QuantityOnHand, 
+			&product.ProductName)
+
+	if err == sql.ErrNoRows {
+		log.Println( "No rows" )
+		return nil, nil
+	} else if err != nil {
+		log.Fatal( err )
+		return nil, err
+	}
+	return product, nil
+
+	/*
 	productMap.RLock()
 	defer productMap.RUnlock()
 	if product, ok := productMap.m[productID]; ok {
 		return &product
 	}
 	return nil
-}
+	*/
+} // getProduct
 
 func removeProduct(productID int) {
+	var funcName = fileNameData + "removeProduct: "
+	log.Printf( "In %s with productID: %d \n", funcName, productID )
 	productMap.Lock()
 	defer productMap.Unlock()
 	delete(productMap.m, productID)
 }
 
 func getProductList() ([]Product, error) {
-
+	var funcName = fileNameData + "getProductList: "
+	log.Println( funcName )
 	results, err := database.DbConn.Query( `select productId, manufacturer, sku, upc, pricePerUnit, quantityOnHand, productName from products` )
 	if err != nil {
 		return nil, err
@@ -80,6 +113,7 @@ func getProductList() ([]Product, error) {
 			&product.QuantityOnHand, 
 			&product.ProductName)
 		products = append( products, product )
+		fmt.Println( "Appended a product to product list: ", strconv.Itoa( product.ProductID ) )
 	}
 	return products, nil
 
@@ -92,7 +126,7 @@ func getProductList() ([]Product, error) {
 	productMap.RUnlock() // why not defer? is that only needed if there is a possible error?
 	return products
 	*/
-}
+} // getProductList
 
 func getProductIds() []int {
 	productMap.RLock()
@@ -111,10 +145,15 @@ func getNextProductID() int {
 }
 
 func addOrUpdateProduct(product Product) (int, error) {
+	var funcName = fileNameData + "addOrUpdateProduct: "
+	log.Println( funcName )
 	// if the product id is set, replace it, otherwise return error
 	addOrUpdateID := -1
 	if product.ProductID > 0 {
-		oldProduct := getProduct(product.ProductID)
+		oldProduct, err := getProduct(product.ProductID)
+		if err != nil {
+			return addOrUpdateID, err
+		}
 		// if it exists, replace it. otherwise return error
 		if oldProduct == nil {
 			return 0, fmt.Errorf("product di [%d] does not exist", product.ProductID)
@@ -128,5 +167,5 @@ func addOrUpdateProduct(product Product) (int, error) {
 	productMap.m[addOrUpdateID] = product
 	productMap.Unlock()
 	return addOrUpdateID, nil
-}
+} // addOrUpdateProduct
 
--- web_services/product/product.service.go
+++ web_services/product/product.service.go
@@ -13,37 +13,63 @@ import (
 
 const productsBasePath = "products"
 
+const fileNameService = "product.service"
+
 func SetupRoutes(apiBasePath string) {
 	handleProducts := http.HandlerFunc(productsHandler)
-	handleProduct := http.HandlerFunc(productHandler)
+	handleProduct  := http.HandlerFunc(productHandler)
+	// "products" handles group
+	// "products/" handles one
 	http.Handle(fmt.Sprintf("%s/%s", apiBasePath, productsBasePath), cors.Middleware(handleProducts))
 	http.Handle(fmt.Sprintf("%s/%s/", apiBasePath, productsBasePath), cors.Middleware(handleProduct))
 }
 
 func productHandler(w http.ResponseWriter, r *http.Request) {
+	var funcName = "In " + fileNameService + ".productHandler: "
+	log.Println( funcName )
 	urlPathSegments := strings.Split(r.URL.Path, "/products/")
-	productID, err := strconv.Atoi(urlPathSegments[len(urlPathSegments)-1])
+	productID, err  := strconv.Atoi(urlPathSegments[len(urlPathSegments)-1])
 
 	if err != nil {
+		log.Print( err )
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
+	/*
 	product := getProduct(productID)
 	if product == nil {
 		w.WriteHeader(http.StatusNotFound)
 		return
 	}
+    */
 	switch r.Method {
 	case http.MethodGet:
 		// return a single product
-		productJSON, err := json.Marshal(product)
+		log.Println( funcName + "In http.MethodGet" )
+		product, err := getProduct( productID ) // json.Marshal(product)
+		if product == nil {
+			log.Println( "Product is nil" )
+			w.WriteHeader(http.StatusNotFound)
+			return
+		}
 		if err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			return
 		}
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(productJSON)
+		j, err2 := json.Marshal( product )
+		if err2 != nil {
+			log.Print( err )
+			w.WriteHeader(http.StatusBadRequest)
+			return
+		}
+		_, err2 = w.Write( j )
+		if err2 != nil {
+			log.Fatal( err2 )
+		}
+		// w.Header().Set("Content-Type", "application/json")
+		// w.Write(productJSON)
 	case http.MethodPut:
+		log.Println( funcName + "In http.MethodPut" )
 		// update a product
 		var updatedProduct Product
 		bodyBytes, err := ioutil.ReadAll(r.Body)
@@ -80,6 +106,8 @@ func productHandler(w http.ResponseWriter, r *http.Request) {
 } // productHandler
 
 func productsHandler(w http.ResponseWriter, r *http.Request) {
+	var funcName = "In " + fileNameService + ".productsHandler: "
+	log.Println( funcName )
 	switch r.Method {
 	case http.MethodGet:
 		productList, errP := getProductList()
